b80c185c1c8850e71e8a7fc269afd3b2acf53419
Anudeep Marella <marella.anudeep@gmail.com>
2015-12-08 18:47:14 -0800

    Add GlobalStatics.java -> Access static variables in a single class through out application.

---
+++ app/src/main/java/com/example/anudeepmarella/customlistview/GlobalStatics.java
@@ -0,0 +1,9 @@
+package com.example.anudeepmarella.customlistview;
+
+/**
+ * Created by anudeepmarella on 12/8/15.
+ */
+public class GlobalStatics {
+
+    public static final String INTENT_EXTRAS_IMAGE_NAME="IMAGE_ID";
+}
